16eca3a1b16b7d21d409516f9e8fc8fee134d8cd
Daniel Baker <danpbaker90@gmail.com>
2016-08-05 22:10:01 +0100

    Setup environment, commit before styles module.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'babel-polyfill';
+import { Router, browserHistory } from 'react-router'
+import { render } from 'react-dom';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+	<Router history={browserHistory} routes={routes} />,
+	document.getElementById('app')
+);
